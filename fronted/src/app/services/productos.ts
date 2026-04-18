f530222ff08ba01ab68a5437acabd3b7578a47e9
Fer Mejia <mariferrmejia@gmail.com>
2026-04-18 17:24:46 -0600

    Subir Front a internet

--- fronted/src/app/services/productos.ts
+++ fronted/src/app/services/productos.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class ProductosService {
   private http = inject(HttpClient); // Inject
-  private url = 'https://finca-sho6.onrender.com'; // Ruta del backend
+  private url = 'https://finca-sho6.onrender.com'; 
 
   getProductos(): Observable<any[]> {
     return this.http.get<any[]>(this.url);
