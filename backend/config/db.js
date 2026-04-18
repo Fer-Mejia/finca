a6db123acbf3e70aa30390c90f723efde26edd19
Fer Mejia <mariferrmejia@gmail.com>
2026-04-18 16:43:28 -0600

    Subir Proyecto

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,11 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 12722, // Agregamos el puerto
+  ssl: {
+    rejectUnauthorized: false // ¡ESTO ES VITAL para Aiven y Render!
+  }
 });
 
 connection.connect(err => {
@@ -13,7 +17,7 @@ connection.connect(err => {
     console.error('Error de conexion:', err);
     return;
   }
-  console.log('Conectado a la base de datos');
+  console.log('Conectado a la base de datos de Aiven');
 });
 
 module.exports = connection;
